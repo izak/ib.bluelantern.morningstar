299b2e3afd21761a63e58829a21cc5d8f18f5b10
Izak Burger <isburger@gmail.com>
2016-03-20 21:18:34 +0200

    Implement modbus query of values.

--- ib/bluelantern/morningstar/mppt.py
+++ ib/bluelantern/morningstar/mppt.py
@@ -1,30 +1,46 @@
+import logging
 from time import time, sleep
 from random import randint
 from threading import Thread
 import paho.mqtt.client as mqtt
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
-def main(host, port, instance, name):
+logger = logging.getLogger(__name__)
+
+def main(host, port, mqtt_username, mqtt_password, instance, name):
+    modbus = ModbusClient(method='rtu', port=port, baudrate=9600, timeout=1)
+
+    if not modbus.connect():
+        logger.error("Cannot connect to modbus")
+        return
+
     client = mqtt.Client()
     client.loop_start()
 
     client.connect(host, port, 60)
+    if mqtt_username is not None:
+        client.username_pw_set(mqtt_username, mqtt_password)
+
     try:
         while True:
             now = int(time())
-            pv_power = 600 + randint(-30, 35)
-            bat_temp = 25 + randint(-1, 1)
-            bat_volt = (245 + randint(-1, 1))/10.0
+            r = modbus.read_holding_registers(0, 16, unit=1)
 
-            client.publish('{}/{}/power'.format(instance, name), "{} {}".format(now, pv_power), 0)
-            client.publish('{}/{}/voltage'.format(instance, name), "{} {}".format(now, bat_volt), 0)
-            client.publish('{}/{}/ampere'.format(instance, name), "{} {}".format(now, pv_power/bat_volt), 0)
+            bat_volt = r.registers[8] * 96.667 * 2**(-15)
+            bat_curr = r.registers[12] * 316.67 * 2**(-15)
+            bat_temp = r.registers[15]
+
+            client.publish('{}/{}/power'.format(instance, name), "{} {:0.2f}".format(now, bat_volt * bat_curr), 0)
+            client.publish('{}/{}/voltage'.format(instance, name), "{} {:0.2f}".format(now, bat_volt), 0)
+            client.publish('{}/{}/current'.format(instance, name), "{} {:0.2f}".format(now, bat_curr), 0)
             client.publish('{}/{}/temperature'.format(instance, name), "{} {}".format(now, bat_temp), 0)
 
-            sleep(1)
+            sleep(5)
     except KeyboardInterrupt:
         pass
     client.disconnect()
     client.loop_stop()
+    modbus.close()
 
 def includeme(config):
     mqtt_host = config.registry.settings.get('mqtt.host')
@@ -32,7 +48,12 @@ def includeme(config):
     instance = config.registry.settings.get('morningstar.instance', 'battery01')
     name = config.registry.settings.get('morningstar.name', 'mppt')
 
+    serial_port = config.registry.settings.get('morningstar.port')
+
     target = lambda: main(mqtt_host, mqtt_port, instance, name)
     thread = Thread(target = target)
     thread.daemon = True
     thread.start()
+
+if __name__ == "__main__":
+    main('localhost', 1883, None, None, 'battery01', 'mppt', sys.argv[1])
